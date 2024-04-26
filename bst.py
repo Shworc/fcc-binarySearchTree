5027521440cb7100ec12ed636f40674f731f76b2
shworc <ojadekses@gmail.com>
2024-04-26 15:23:08 +0200

    Init of BST, creating TreeNode class and bst method

---
+++ bst.py
@@ -0,0 +1,12 @@
+class TreeNode:    # Creating and initializing TreeNode class
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None 
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+        def insert(self, key):   # define a mechanism to insert nodes in the tree
+            pass
